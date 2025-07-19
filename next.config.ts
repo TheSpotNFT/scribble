70e2669bbb523d0368f79ae26ae9eb20b9d24753
thejunglemarket <96501055+TheSpotNFT@users.noreply.github.com>
2025-07-19 09:48:43 -0700

    Update sidebar to show named collections (e.g. Mind Matter)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: 'export', // 👈 Add this line for Netlify static export
 };
 
 export default nextConfig;
